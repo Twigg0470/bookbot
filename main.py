39c3b60e888a7c7471e64d0ca3dacd63d8ef8c79
Twigg0470 <mike.stu.willis@icloud.com>
2025-06-26 10:26:26 +0100

    added word ccount, char count and report format

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words, character_repeat_count
+from stats import count_words, character_repeat_count, sort_dict
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,9 +8,24 @@ def get_book_text(file_path):
     
 
 def main():
-    count = count_words(get_book_text("books/frankenstein.txt"))
-    print(f"{count} words found in the document")
-    print(character_repeat_count(get_book_text("books/frankenstein.txt")))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}")
+
+    text = get_book_text(sys.argv[1])
+
+    print("----------- Word Count ----------")
+    count = count_words(text)
+    print(f"Found {count} total words")
+    
+    sorted_list = sort_dict(character_repeat_count(text))
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        print(f"{item['char']}: {item['num']}")
+   
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,24 @@ def character_repeat_count(text):
             letter_int[i] += 1
     return letter_int
 
+def sort_dict(dictionary):
+    sorted_list = []
+    def sort_on(items):
+        return items["num"]
+    
+    for key, value in dictionary.items():
+        if key.isalpha() == True:
+            new_dict = {}
+            new_dict["char"] = key
+            new_dict["num"] = value
+
+            sorted_list.append(new_dict)
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+            
+
+
+        
+
+
